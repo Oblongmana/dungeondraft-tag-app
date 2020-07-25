a32f6202340e2cbcb8d44bd82ebfa49e94a5d711
James Hill <me@jameshill.io>
2020-07-25 21:45:43 +1200

    Updates

---
+++ precache-manifest.120035dc02115f6c9a370d72a0597850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba6474e69e0e2c42165f8f0c5452130",
+    "url": "/dungeondraft-tag-app/index.html"
+  },
+  {
+    "revision": "100d5dfced51e25a51a0",
+    "url": "/dungeondraft-tag-app/static/css/main.1d0654d1.chunk.css"
+  },
+  {
+    "revision": "85f9e724be6f7e9d98b8",
+    "url": "/dungeondraft-tag-app/static/js/2.48b9541a.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/dungeondraft-tag-app/static/js/2.48b9541a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100d5dfced51e25a51a0",
+    "url": "/dungeondraft-tag-app/static/js/main.b8ec0e01.chunk.js"
+  },
+  {
+    "revision": "1c200665b5f60e8d822c",
+    "url": "/dungeondraft-tag-app/static/js/runtime-main.4a41d901.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dungeondraft-tag-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
